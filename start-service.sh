d1badc225edf05db4908c6caa17bdf30738613bf
Felipe F Quintella <felipe.quintella@fgv.br>
2016-02-19 14:45:52 -0200

    FIX

--- start-service.sh
+++ start-service.sh
@@ -4,5 +4,11 @@ set -e
 
 /opt/puppetlabs/puppet/bin/puppet apply -l /tmp/puppet.log --modulepath=/etc/puppet/modules /etc/puppet/manifests/start.pp
 
+while [ ! -f ${FACTER_BAMBOO_HOME}/logs/atlassian-bamboo.log ]
+do
+  sleep 2
+done
+ls -l ${FACTER_BAMBOO_HOME}/logs/atlassian-bamboo.log
+
 tail -n 0 -f ${FACTER_BAMBOO_HOME}/logs/atlassian-bamboo.log &
 wait
